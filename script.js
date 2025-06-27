25521c5f3de3241ce57e2b70c4364fe8b881cf7a
Pedro Wantuir <pedrowantuirpaixao@gmail.com>
2025-06-27 02:01:45 -0300

    navbar responsiva implementada

---
+++ script.js
@@ -0,0 +1,9 @@
+const iconeMenu = document.querySelector('.icone-menu');
+const mainNav = document.getElementById('main_nav');
+
+iconeMenu.addEventListener('click', () => {
+    mainNav.classList.toggle('active');
+    iconeMenu.classList.toggle('open');
+});
+
+
